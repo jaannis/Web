eb1fd44a7e6bb4a86896cb91f2497c95169bc951
jaannis <janis.zalitis.ba@gmail.com>
2018-01-31 20:45:08 +0200

    web5

---
+++ web5/main.js
@@ -0,0 +1,22 @@
+$('#registration-form').submit(function (event) {
+    var hasErrors = false;
+    $('.errorText').remove();
+
+    var $userName = $('#registration-form input[name="username"]');
+    if ($userName.val() == '') {
+        hasError = true;
+        var error = '<div class="errorText">Field <b> Username</b> is empty</div>';
+        $userName.parent().append(error);
+    }
+    var emailElement = 'input[name="email"]';
+    var $email = $(this).find(emailElement);
+    if ($email.val() == '') {
+        hasError = true;
+        var error2 = '<div class="errorText">Field <b> Email</b> is empty</div>';
+        $email.parent().append(error2);
+    }
+    if (hasError == true) {
+        event.preventDefault();
+
+    }
+});
